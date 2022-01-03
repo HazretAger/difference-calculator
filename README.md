dd79af2e0c3c2deefa10a57fc91c901463c13d3a
Xazret <HazAger777@yanddex.ru>
2022-01-03 19:53:08 +0300

    added test, connected CodeClimate

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/HazretAger/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/HazretAger/frontend-project-lvl2/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/183f29f111531eb2baf3/maintainability)](https://codeclimate.com/github/HazretAger/frontend-project-lvl2/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/183f29f111531eb2baf3/test_coverage)](https://codeclimate.com/github/HazretAger/frontend-project-lvl2/test_coverage)
 
 ### Comparison of flat JSON files
 [![asciicast](https://asciinema.org/a/HB02J9fALOcq8i1bpLk1PLZah.svg)](https://asciinema.org/a/HB02J9fALOcq8i1bpLk1PLZah)
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,18 @@
-/*import getDiff from '../src/render.js';
+import { readFileSync } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import getDiff from '../src/render.js';
 
-test('difference', () => {
-    expect('Hello').toEqual('Hello');
-})*/
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('comparison of flat JSON', () => {
+    const filepath1 = getFixturePath('beforeFlat1.json');
+    const filepath2 = getFixturePath('beforeFlat2.json');
+    const result = readFileSync(getFixturePath('afterFlatJson.txt'), 'utf-8');
+
+    expect(getDiff(filepath1, filepath2)).toEqual(result);
+})
