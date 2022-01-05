33bb1604e75402304e6fd7bc4789c6b9b6f31d49
Xazret <HazAger777@yanddex.ru>
2022-01-05 21:32:20 +0300

    improved extension

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import getDiff from '../src/index';
+import getDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
